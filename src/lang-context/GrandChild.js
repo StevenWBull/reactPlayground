e4ad3259f94ee4dcc1e55e19d9494d5c7202b07b
Steven Bull <stevenwbulljr@gmail.com>
2019-09-11 20:14:10 -0500

    add javascript to GrandChild.js

---
+++ src/lang-context/GrandChild.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import GreatGrandChild from './GreatGrandChild';
+
+export default function GrandChild() {
+  return (
+    <div className='GrandChild'>
+      <GreatGrandChild />
+    </div>
+  )
+}
